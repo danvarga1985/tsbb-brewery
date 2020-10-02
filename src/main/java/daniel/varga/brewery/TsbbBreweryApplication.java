47144329c9a5d47bcdc5d009990143ce37d72c8a
danvarga1985 <danvarga1985@gmail.com>
2020-10-02 08:23:33 +0200

    Initial commit

---
+++ src/main/java/daniel/varga/brewery/TsbbBreweryApplication.java
@@ -0,0 +1,13 @@
+package daniel.varga.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsbbBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsbbBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daniel/varga/brewery/TsbbBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.varga.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsbbBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
